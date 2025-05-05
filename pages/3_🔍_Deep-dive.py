26bfa516ed442e5a2d74982dda2caea95bbba78c
Ruben Cardell <rucani@users.noreply.github.com>
2025-05-05 19:04:46 +0200

    Updated Deep-dive

--- pages/3_🔍_Deep-dive.py
+++ pages/3_🔍_Deep-dive.py
@@ -14,61 +14,86 @@ st.logo("Logo.png", size="large")
 st.title("🔍 Deep-dive - Über HitPredict")
 
 st.markdown("""
-HitPredict ist eine datengetriebene Applikation, die es Künstlern und Produzenten ermöglicht, das Erfolgspotenzial eines Songs objektiv einzuschätzen – noch bevor dieser offiziell veröffentlicht wird!  
-Im Zentrum des Tools stehen moderne Methoden der Audio- und Textanalyse sowie maschinelles Lernen verbunden mit APIs.
-
----
-""")
-
-st.subheader("📊 Analyseprozess")
-
-st.markdown("""
-Der Analyseprozess beginnt, sobald ein Nutzer eine Audiodatei – im MP3-Format – auf unserer Plattform hochlädt.  
-Im ersten Schritt werden eine Vielzahl von Audioeigenschaften extrahiert:  
-- Tanzbarkeit, Energie, Schlüssel, Lautstärke, Tonart
-- Sprachlichkeit, Akustizität, Instrumentalität
-- Wertigkeit, Tempo, Dauer, Taktart
-- Wiederholung, Lesbarkeit, Stimmung, Eindeutigkeit
-
-Die Extraktion erfolgt automatisiert über spezialisierte Audioanalyse-Frameworks wie **Essentia**.
-
-Parallel dazu wird mit **OpenAI Whisper** eine automatische Transkription des Songtexts durchgeführt.  
-Die gewonnenen Lyrics werden analysiert hinsichtlich:
-- Wortvielfalt (Vocabulary Diversity)
-- Lesbarkeit (Readability)
-- Emotionales Sentiment (Positivity/Negativity) mittels TextBlob
-- Jugendfreiheit (Profanity Analyse)
-- Repetitivität
-- Sprachliche Komplexität
+**HitPredict** ist eine datengetriebene Applikation, die es Künstlern und Produzenten ermöglicht, das **Erfolgspotenzial** 
+eines Songs objektiv einzuschätzen – noch bevor dieser offiziell veröffentlicht wird!  
+Im Zentrum des Tools stehen moderne Methoden der **Audio- und Textanalyse** sowie **maschinelles Lernen** und **APIs**.
 
 ---
 """)
 
 st.subheader("🗃️ Unser Datenset")
 
+st.markdown("""
+Unsere Vorhersage basiert auf einem [Kaggle Datensatz](https://www.kaggle.com/datasets/amitanshjoshi/spotify-1million-tracks) 
+von ursprünglich über **1.160.000 Songs**, die über die [**Spotify Web API**](https://developer.spotify.com/documentation/web-api) 
+abgerufen wurden.
+
+Die Songs enthalten von Spotify berechnete Attribute, unter anderem
+- **Popularity**: Beliebtheit des Songs, errechnet durch Aufrufe und Alter.
+- **Year**: Jahr des Releases. Es sind ausschliesslich Songs von 2000-2023 enthalten.
+- **Genre**: Genre des Songs. Alle Genres, die nicht sowohl von Essentia als auch der Spotify Web API erkannt werden, sind 'other'.
+- **Danceability**: Tanzbarkeit basierend auf einer Kombination von Tempo, Rhythmus, Taktstärke und Regelmässigkeit.
+- **Energy**: Mass für Intensität und Aktivität. Typischerweise fühlen sich energiegeladene Tracks schnell, laut und geräuschvoll an.
+- **Key**: Tonart, in der sich der Track befindet. Verwendet Pitch Class notation.
+- **Loudness**: Gesamtlautstärke eines Songs in Dezibel (dB).
+- **Mode**: Modalität (Dur oder Moll) eines Songs, also die Art der Tonleiter.
+- **Speechiness**: Vorhandensein von gesprochenen Wörtern in einem Track.
+- **Acousticness**: Konfidenzniveau das angibt, ob der Track rein akustisch ist.
+- **Instrumentalness**: Zeigt an, ob ein Song keinen Gesang enthält.
+- **Valence**: Positivität des Songs. Hohe Valenz klingt glücklich und euphorisch, während niedrige Valenz eher traurig und wütend ist.
+- **Tempo**: Das geschätzte Gesamttempo eines Tracks in Beats pro Minute (BPM).
+- **Time signature**: Geschätzte Taktart. Gibt an, wie viele Schläge ein Takt hat.
+- **Duration**: Dauer des Tracks in Millisekunden.
+
+Um zusätzlich auch textbasierte Attribute zu verwenden, haben wir die **Lyrics** für alle 1.160.000 Songs über die 
+[**LRCLIB API**](https://lrclib.net/) abgerufen. Für **560.000 Songs** war dies erfolgreich, alle anderen haben wir aussortiert.
+
+Anhand der Lyrics konnten wir folgende **textbasierte Metriken** berechnen:
+- **Repetition**: Wiederholungen basierend auf 3er N-Grams, also die Anzahl an identischen 3-Wort-Abschnitten.
+- **Readability**: Lesbarkeit nach der Dale–Chall Formel. Mass für die Verständnisschwierigkeiten beim Lesen eines Textes.
+- **Polarity**: Polarität der Lyrics. Beschreibt positive oder negative Stimmung.
+- **Subjectivity**: Subjektivität misst den Anteil der persönlichen Meinung.
+- **Explicitness**: Obszönität oder anstössige Sprache in den Lyrics.
+
+So entstand unser finaler Datensatz für das Machine Learning *(Auszug)*:
+""")
+
 df = load_dataset()
 st.dataframe(df)
 
 st.divider()
 
+
 st.subheader("🧠 Machine Learning Modell")
+st.markdown("""Um Machine Learning Modelle zu trainieren, haben wir zunächst einige Ausreisser entfernt, 
+wie z.B. Songs mit einer Time signature von 0 (173 Stück). Ausserdem haben wir One-Hot Encoding für die Genres angewendet, 
+damit diese als numerische Werte für das Modell lesbar sind. Zudem wurden alle Werte mit dem scikit-learn Standardscaler standardisiert.
 
-st.markdown("""
-Die rund 20 extrahierten Audio- und Textfeatures werden an ein Machine-Learning-Modell übergeben:  
-- **XGBoost (Extreme Gradient Boosting)** – robustes Verfahren speziell für Regressionsaufgaben
-- Trainiert auf ca. **560.000 Spotify-Songs** (2000–2023)
-- Zielwert: **Spotify Popularity Score** (Skala 0–100)
+- **X**: Alle Metriken exkl. Popularity
+- **Y**: Popularity Score (0–100)
 
-Starke Prädiktoren:
-- Genre
-- Instrumentalität
-- Tanzbarkeit
-- Readability
-- Sentiment
-
-Eine Visualisierung der Feature-Importance zeigt nachvollziehbar, worauf das Modell bei der Popularitätsschätzung achtet.
-
+Folgende Machine Learning Modelle haben wir trainiert (teils mit Hyperparameter-Tuning):
+- **Lineare Regression** (mit **Rigde** und **Lasso** Regularisation)
+- **Random Forest**
+- **XGBoost**
+- **Neuronales Netzwerk**
 """)
+
+ml_results = pd.DataFrame({
+    "Lineare Regression": ["12.34", "9.73"],
+    "LinReg Ridge": ["12.25", "9.69"],
+    "LinReg Lasso": ["12.25", "9.69"],
+    "Random Forest": ["11.44", "8.99"],
+    "**XGBoost**": ["**11.38**", "**8.93**"],
+    "Neural Network": ["11.42", "9.04"],
+    },
+index=["RMSE", "MAE"]
+)
+st.table(ml_results)
+
+st.markdown("Da XGBoost die besten Resultate zeigte, haben wir das Modell für HitPredict ausgesucht.")
+
+st.divider()
 
 col1, col2 = st.columns(2)
 
@@ -76,39 +101,98 @@ with col1:
     st.subheader("❗ Wichtigkeit der Features")
     st.image("img/Feature Importance XGBoost.png", width=750)
 
+    st.markdown("""
+    Anhand der **Feature-Importance** erkennt man, welche Metriken bei der Popularity-Vorhersage am wichtigsten sind.
+    
+    **Achtung**: Dies zeigt lediglich, wie stark die einzelnen Metriken berücksichtigt werden.
+    Über einen positiven/negativen Einfluss kann keine Aussage gemacht werden.
+    - Genre
+    - Year
+    - Instrumentalness
+    - Danceability
+    """)
+
 with col2:
     st.subheader("🎯 Genauigkeit unseres Modells")
     st.image("img/XGBoost Accuracy.png", width=600)
+
+    st.markdown("""
+    Die **Genauigkeit** unseres Modells haben wir ebenfalls visualisiert.
+    
+    Hier erkennt man, dass das Modell **niedrige Werte überschätzt**, während es **hohe Werte überschätzt**.
+    Das Modell bewegt sich so im Zweifel weg von den Extremen, um hohe Fehler zu vermeiden.
+    """)
+
+st.divider()
+
+
+st.subheader("📊 Analyseprozess")
+
+st.markdown("""
+Sobald ein Nutzer eine **Audiodatei** – im MP3-Format – auf unserer Plattform hochlädt, extrahiert die Python Library 
+**Essentia** alle oben genannten Metriken aus dem Audio.
+
+[**Essentia**](https://essentia.upf.edu/essentia_python_tutorial.html) ist eine open-source C++ Library, 
+die von der Universität Pompeu Fabra (UPF) für die **Audioanalyse** entwickelt wurde.
+Für Essentia existieren Python-Bindings und eine Tensorflow-Erweiterung mit vortrainierten Machine Learning Modellen.
+Essentia berechnet die Metriken wie folgt:
+ 
+**Standard-Essentia**
+- Duration, Tempo, Key, Mode, Loudness, Energy, Danceability und Time signature
+
+**Tensorflow-Erweiterung**
+- Genre, Acousticness, Instrumentalness, Speechiness und Valence
+
+
+Anschliessend wird mit **OpenAI Whisper** – das über die [**Deepgram API**](https://developers.deepgram.com/docs/deepgram-whisper-cloud) 
+läuft – eine automatische Transkription der **Lyrics** durchgeführt.
+
+Die textbasierten Metriken werden mit den selben Methoden berechnet, wie für den Datensatz zuvor.
+- [**TextBlob**](https://textblob.readthedocs.io/en/dev/): Repetition, Polarity, Subjectivity
+- [**Textstat**](https://textstat.org/): Readability
+- [**profanity-check**](https://github.com/vzhou842/profanity-check): Explicitness
+""")
 
 st.divider()
 
 st.subheader("📈 Ergebnisausgabe")
 
+col1, col2 = st.columns([0.6,0.4])
+
+with col1:
+    st.markdown("""
+    Nach der Analyse liefert HitPredict eine Popularitätsschätzung von **0 bis 100 Punkten**:
+    
+    - **< 30 Punkte** → kein Hitpotenzial
+    - **30–50 Punkte** (Top 40%) → durchschnittliches Potenzial
+    - **50–60 Punkte** (Top 9%) → Wahrscheinlichkeit für einen Hit steigt deutlich
+    - **60+ Punkte** (Top 3%) → hohe Wahrscheinlichkeit für viralen Erfolg
+    
+    Ein **Netzdiagramm** zeigt anschliessend, welche Metriken verbessert werden können, 
+    um die Erfolgschancen eines Songs zu steigern.
+    """)
+
+with col2:
+    st.image("img/Popularity Distribution.png")
+
+st.divider()
+
+st.subheader("👫 Song Soulmate")
+
 st.markdown("""
-Nach der Analyse liefert HitPredict eine Popularitätsschätzung von **0 bis 100 Punkten**:
+Naheliegend wäre doch auch, die berechneten Metriken mit jedem Song des Datensatzes zu **vergleichen**, 
+um einen Song zu identifizieren, der dem hochgeladenen Audio am ähnlichsten ist.
+– Und genauso machen wir es!
 
-- **< 30 Punkte** → kein Hitpotenzial
-- **30–50 Punkte** → durchschnittliches Potenzial
-- **50–60+ Punkte** → Wahrscheinlichkeit für einen Hit steigt deutlich
-- **60+ Punkte** → Top 3% der Songs – hohe Wahrscheinlichkeit für viralen Erfolg
+Wir berechnen dafür die **geometrische Distanz** im 44-dimensionalen Raum zwischen dem Vektor der standardisierten Metriken 
+des hochgeladenen Audios und den jeweiligen Vektoren aller anderen Songs im Datensatz.
+Der niedrigste Wert wird als **bestes Match** identifiziert.
 
-Ein **Netzdiagramm** zeigt, welche Metriken verbessert werden können, um die Erfolgschancen eines Songs zu steigern.
-
----
+Mittels einer Anfrage an die **Spotify Web API** wird die aktuelle Popularity des Matches abgerufen und zusammen 
+mit einem abspielbaren Widget angezeigt.
 """)
 
-st.subheader("🤖 Technologien hinter HitPredict")
-
-st.markdown("""
-- **Python** für Programmierung
-- **Streamlit** als Benutzeroberfläche
-- **Whisper** für Transkription
-- **Essentia** für Audioanalyse
-- **TextBlob**, **textstat** für Textanalysen
-- **scikit-learn**, **XGBoost** für Machine Learning
-
----
-""")
+st.divider()
 
 st.subheader("🎯 Unser Ziel")
 
@@ -117,6 +201,6 @@ Mit HitPredict bieten wir Künstlerinnen und Künstlern ein Tool, das datenbasie
 Ob bei der Auswahl einer Single, der Einschätzung von Marketingbudgets oder der Priorisierung von Albumtracks –  
 **HitPredict liefert eine fundierte Zweitmeinung auf Basis realer Musik- und Textdaten.**
 
----
+#### HitPredict: *Know your hit before it’s heard.*
 """)
 
